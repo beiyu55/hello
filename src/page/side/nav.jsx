750417561dd0606e798e6ff8e7e3f607edfb1cd0
hubwei <1124725517@qq.com>
2019-03-13 20:37:50 +0800

    样式小修改

--- src/page/side/nav.jsx
+++ src/page/side/nav.jsx
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink
-} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { getComponentLocale } from '../../i18n/getLocale';
-import MyReact from '../api/myReact';  // 导入App组件
-import Components from '../api/components';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import { getComponentLocale } from "../../i18n/getLocale";
+import MyReact from "../api/myReact"; // 导入App组件
+import Components from "../api/components";
 
 class SideNav extends Component {
   // 获取 context
   static contextTypes = {
-    locale: PropTypes.object,
-  }
+    locale: PropTypes.object
+  };
 
   render() {
-    const locale = getComponentLocale(this.props, this.context, 'i18n_nav');
+    const locale = getComponentLocale(this.props, this.context, "i18n_nav");
     return (
       <Router>
         <div className="main">
           <ul className="nav">
-            <li><NavLink to="/" exact={true} activeClassName="selected">{locale.abstract}</NavLink></li>
-            <li><NavLink to="/component" activeClassName="selected">{locale.component}</NavLink></li>
+            <li>
+              <NavLink to="/" exact={true} activeClassName="selected">
+                {locale.abstract}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/component" activeClassName="selected">
+                {locale.component}
+              </NavLink>
+            </li>
           </ul>
           <div className="content">
             <Route exact path="/" component={MyReact} />
@@ -30,8 +34,8 @@ class SideNav extends Component {
           </div>
         </div>
       </Router>
-    )
+    );
   }
 }
 
-export default SideNav
+export default SideNav;
